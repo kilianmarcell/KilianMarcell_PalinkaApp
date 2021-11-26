52e89698a1fa5c491eb0549d6ff7279cf7a89959
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-11-26 17:54:53 +0100

    Main activityhez Pálinka felvétele gomb hozzáadva

--- app/src/main/java/hu/petrik/palinkaapp/MainActivity.java
+++ app/src/main/java/hu/petrik/palinkaapp/MainActivity.java
@@ -3,12 +3,21 @@ package hu.petrik.palinkaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAdatFelvetelMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    public void init() {
+        btnAdatFelvetelMain = findViewById(R.id.btnAdatFelvetelMain);
     }
 }
