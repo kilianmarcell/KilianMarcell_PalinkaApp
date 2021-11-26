3e5238bbdafd69e4f2ebace37c20b3fb8a82a1dc
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-11-26 18:02:29 +0100

    Gombok átmennek az activityk-re

--- app/src/main/java/hu/petrik/palinkaapp/MainActivity.java
+++ app/src/main/java/hu/petrik/palinkaapp/MainActivity.java
@@ -2,12 +2,16 @@ package hu.petrik.palinkaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnAdatFelvetelMain, btnPalinkaKeresese;
+    private Button btnAdatFelvetelMain, btnPalinkaKeresese, btnPalinkaListazasa;
+    private TextView txtVListaz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         init();
+
+        btnAdatFelvetelMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AdatFelvetelActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnPalinkaKeresese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AdatKeresesActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void init() {
         btnAdatFelvetelMain = findViewById(R.id.btnAdatFelvetelMain);
         btnPalinkaKeresese = findViewById(R.id.btnPalinkaKeresese);
+        btnPalinkaListazasa = findViewById(R.id.btnPalinkaListazasa);
+        txtVListaz = findViewById(R.id.txtVListaz);
     }
 }
