8995a1d1d68b20fa2fe43c5eb0bfe4a2b49be128
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-11-27 13:36:16 +0100

    DBHelper hozzáadva

--- app/src/main/java/hu/petrik/palinkaapp/AdatFelvetelActivity.java
+++ app/src/main/java/hu/petrik/palinkaapp/AdatFelvetelActivity.java
@@ -2,13 +2,49 @@ package hu.petrik.palinkaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AdatFelvetelActivity extends AppCompatActivity {
+
+    private EditText eTFozoAdatFelvetel, eTGyumolcsAdatFelvetel, eTAlkoholTartalomAdatFelvetel;
+    private Button btnFelvetelAdatFelvetel, btnVisszaAdatFelvetel;
+    private String fozoAdat = eTFozoAdatFelvetel.getText().toString();
+    private String gyumolcsAdat = eTFozoAdatFelvetel.getText().toString();
+    private String alkoholTartalomAdat = eTFozoAdatFelvetel.getText().toString();
+    private boolean kivantetoltve = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adat_felvetel);
+
+        init();
+
+        btnFelvetelAdatFelvetel.setOnClickListener(view -> {
+            if (!(fozoAdat == "" && gyumolcsAdat == "" && alkoholTartalomAdat == "")) {
+                kivantetoltve = true;
+            }
+        });
+
+        btnVisszaAdatFelvetel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AdatFelvetelActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void init() {
+        eTFozoAdatFelvetel = findViewById(R.id.eTFozoAdatFelvetel);
+        eTGyumolcsAdatFelvetel = findViewById(R.id.eTGyumolcsAdatFelvetel);
+        eTAlkoholTartalomAdatFelvetel = findViewById(R.id.eTAlkoholTartalomAdatFelvetel);
+        btnFelvetelAdatFelvetel = findViewById(R.id.btnFelvetelAdatFelvetel);
+        btnVisszaAdatFelvetel = findViewById(R.id.btnVisszaAdatFelvetel);
     }
 }
